01e73ee80bd3cda7dcdc6ec81d9bd84139a8cf26
emmaikonen <emma.ikonen@tuni.fi>
2020-12-05 16:38:14 +0200

    Create capitalize.test.js

---
+++ src/Test/capitalize.test.js
@@ -0,0 +1,14 @@
+import chai from "chai"
+import capitalize from "../capitalize.js"
+
+const expect = chai.expect 
+
+const users = "FRED"
+
+const answer = "Fred"
+
+describe("capitalize", () => {
+    it("Converts the first character of `string` to upper case and the remaining to lower case.", () =>{
+        expect(filter(users)).to.eql(answer)
+    });
+})
