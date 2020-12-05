cfd054406c8a7700afb86e92aa0e17c6479bf304
Venla Viertola <venla.viertola@tuni.fi>
2020-12-05 16:20:48 +0200

    tests completed and modified

--- src/Test/compact.test.js
+++ src/Test/compact.test.js
@@ -7,7 +7,7 @@ const value = [0, 1, false, 2, '', 3]
 const answer = [1, 2, 3]
 
 describe("compact", () => {
-    it("cast array", () =>{
+    it("Creates an array with all falsey values removed.", () =>{
         expect(compact(value)).to.eql(answer)
     });
 })
--- src/Test/eq.test.js
+++ src/Test/eq.test.js
@@ -5,11 +5,10 @@ const expect = chai.expect
 
 const object = { 'a': 1 }
 
-const other = "true"
 
 describe("eq", () => {
     it("equal", () =>{
-        expect(eq(object, object)).to.eql(answer)
+        expect(eq(object, object)).to.be.true
     });
 })
 
