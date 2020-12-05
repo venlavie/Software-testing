e1ffe9c77bb3a9cdf8cf76d181155499ab379897
emmaikonen <emma.ikonen@tuni.fi>
2020-12-05 16:50:20 +0200

    Create chunk.test.js

---
+++ src/Test/chunk.test.js
@@ -0,0 +1,16 @@
+import chai from "chai"
+import chunk from "../chunk.js"
+
+const expect = chai.expect 
+
+const users = ['a', 'b', 'c', 'd']
+const value = 2
+
+const answer = [['a', 'b'], ['c', 'd']]
+
+describe("chunk", () => {
+    it("Creates an array of elements split into groups the length of `size`.", () =>{
+        expect(chunk(users, value)).to.eql(answer)
+    });
+})
+
