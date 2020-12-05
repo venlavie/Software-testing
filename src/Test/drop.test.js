27f2b1c6215c4f830c193e0ce61d6d980677ca53
emmaikonen <emma.ikonen@tuni.fi>
2020-12-05 16:47:21 +0200

    Update drop.test.js

---
+++ src/Test/drop.test.js
@@ -0,0 +1,14 @@
+import chai from "chai"
+import drop from "../drop.js"
+
+const expect = chai.expect 
+
+const users = [1, 2, 3]
+
+const answer = [2, 3]
+
+describe("drop", () => {
+    it("Creates a slice of `array` with `n` elements dropped from the beginning.", () =>{
+        expect(filter(users)).to.eql(answer)
+    });
+})
